45ecc9031084a7d05034d43c28a02a6c350d0b34
Bochan kim <44803830+bo1030@users.noreply.github.com>
2021-11-07 14:15:21 +0900

    DB 임시 추가

--- server/src/main/java/com/taxi_sharing/server/Controller/test.java
+++ server/src/main/java/com/taxi_sharing/server/Controller/test.java
@@ -1,5 +1,6 @@
 package com.taxi_sharing.server.Controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- server/src/test/java/com/taxi_sharing/server/ServerApplicationTests.java
+++ server/src/test/java/com/taxi_sharing/server/ServerApplicationTests.java
@@ -2,13 +2,13 @@ package com.taxi_sharing.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 
 @SpringBootTest
 class ServerApplicationTests {
 
     @Test
     void contextLoads() {
-
     }
 
 }
