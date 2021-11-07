a23d76c29b7a6669622981e02fe637014f3c318a
Bochan kim <44803830+bo1030@users.noreply.github.com>
2021-11-07 12:23:40 +0900

    서버 추가
    서버 추가

---
+++ src/test/java/com/taxi_sharing/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taxi_sharing.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
