e4be797904df70efa52e90c5a70793d9dd9e6798
huittting <87351610+huittting@users.noreply.github.com>
2022-01-21 18:05:47 +0900

    feat: payment domain initial

---
+++ server/src/main/java/com/taxisharing/server/payment/domain/Payment.java
@@ -0,0 +1,12 @@
+package com.taxisharing.server.payment.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Payment {
+}
