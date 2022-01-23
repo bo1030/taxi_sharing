115734f6c2f251388f869e33c5dc56115b3a4b4f
kjw6867 <93850235+kjw6867@users.noreply.github.com>
2022-01-23 13:42:49 +0900

    feat : chatting room 도메인 추가

---
+++ server/src/main/java/com/taxisharing/server/chat/domain/ChatRoom.java
@@ -0,0 +1,66 @@
+package  com.taxisharing.server.chat.domain;
+import com.taxisharing.server.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "chat_room")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
+public class ChatRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 45, nullable = false)
+    private String hostName;
+
+    private Date generateTime;
+
+    private Date departureTime;
+
+    @Column(length = 64, nullable = false)
+    private String departureAddress;
+
+    private float departureLatitude;
+
+    private float departureLongitude;
+
+    @Column(length = 64, nullable = false)
+    private String destinationAddress;
+
+    private float destinationLatitude;
+
+    private float destinationLongitude;
+
+    private Integer maximumNumber;
+    private Integer currentNumber;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User host;
+
+    public ChatRoom(String hostName, Date generateTime, Date departureTime, String departureAddress, float departureLatitude, float departureLongitude, String destinationAddress, float destinationLatitude, float destinationLongitude, Integer maximumNumber, Integer currentNumber, User host){
+        this.hostName = hostName;
+        this.generateTime = generateTime;
+        this.departureTime = departureTime;
+        this.departureAddress = departureAddress;
+        this.departureLatitude = departureLatitude;
+        this.departureLongitude = departureLongitude;
+        this.destinationAddress = destinationAddress;
+        this.destinationLatitude = destinationLatitude;
+        this.destinationLongitude = destinationLongitude;
+        this.maximumNumber = maximumNumber;
+        this.currentNumber = currentNumber;
+        this.host = host;
+    }
+    public ChatRoom(String hostName, Date generateTime, Date departureTime, String departureAddress, float departureLatitude, float departureLongitude, String destinationAddress, float destinationLatitude, float destinationLongitude, Integer maximumNumber, User host){
+        this(hostName,generateTime,departureTime,departureAddress,departureLatitude,departureLongitude,destinationAddress,destinationLatitude,destinationLongitude,maximumNumber,1, host);
+    }
+}
