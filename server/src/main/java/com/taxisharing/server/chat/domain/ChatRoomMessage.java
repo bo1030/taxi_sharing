ca8c82677e6bde2074aae7e178a7f2d5ff6290be
kjw6867 <93850235+kjw6867@users.noreply.github.com>
2022-01-23 14:47:46 +0900

    feat : chat room message 도메인 추가

---
+++ server/src/main/java/com/taxisharing/server/chat/domain/ChatRoomMessage.java
@@ -0,0 +1,45 @@
+package com.taxisharing.server.chat.domain;
+
+import com.taxisharing.server.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "chat_room_message")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatRoomMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 45, nullable = false)
+    private String text;
+
+    private Date time;
+    private Integer readCount;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom room;
+
+    public ChatRoomMessage(String text, Date time, Integer readCount, User user, ChatRoom room){
+        this.text = text;
+        this.time = time;
+        this.readCount = readCount;
+        this.user = user;
+        this.room = room;
+    }
+    public ChatRoomMessage(String text, Date time, User user, ChatRoom room){
+        this(text, time, 1, user, room);
+    }
+}
