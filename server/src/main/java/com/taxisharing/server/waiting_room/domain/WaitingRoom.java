3ee5632116aab1113793dd1c6bd63079c44add4b
dohkim <dh54kim54@gmail.com>
2022-01-24 23:21:27 +0900

    feat: domain 관계 추가 #16

--- server/src/main/java/com/taxisharing/server/waiting_room/domain/WaitingRoom.java
+++ server/src/main/java/com/taxisharing/server/waiting_room/domain/WaitingRoom.java
@@ -1,5 +1,6 @@
 package com.taxisharing.server.waiting_room.domain;
 
+import com.taxisharing.server.user.domain.User;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class WaitingRoom {
@@ -15,6 +17,10 @@ public class WaitingRoom {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User host;
 
     @Column(length = 16, nullable = false)
     private String hostName;
@@ -35,16 +41,22 @@ public class WaitingRoom {
     @Column(name = "departure_longitude", nullable = false)
     private Float departureLongitude;
 
+    @Column(length = 64, nullable = false)
+    private String destinationAddress;
+
+    @Column(name = "destination_latitude", nullable = false)
+    private Float destinationLatitude;
+    @Column(name = "destination_longitude", nullable = false)
+    private Float destinationLongitude;
+
     @Column(name = "maximum_number", nullable = false)
     private Integer maximumNumber;
     @Column(name = "current_number", nullable = false)
     private Integer currentNumber;
 
-    public WaitingRoom(Integer id, String hostName, Integer status,
-                       LocalDateTime generateTime, LocalDateTime departureTime,
-                       String departureAddress, Float departureLatitude, Float departureLongitude,
-                       Integer maximumNumber, Integer currentNumber) {
+    public WaitingRoom(Integer id, User host, String hostName, Integer status, LocalDateTime generateTime, LocalDateTime departureTime, String departureAddress, Float departureLatitude, Float departureLongitude, String destinationAddress, Float destinationLatitude, Float destinationLongitude, Integer maximumNumber, Integer currentNumber) {
         this.id = id;
+        this.host = host;
         this.hostName = hostName;
         this.status = status;
         this.generateTime = generateTime;
@@ -52,6 +64,9 @@ public class WaitingRoom {
         this.departureAddress = departureAddress;
         this.departureLatitude = departureLatitude;
         this.departureLongitude = departureLongitude;
+        this.destinationAddress = destinationAddress;
+        this.destinationLatitude = destinationLatitude;
+        this.destinationLongitude = destinationLongitude;
         this.maximumNumber = maximumNumber;
         this.currentNumber = currentNumber;
     }
