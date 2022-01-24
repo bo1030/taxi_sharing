70832bc560d3975de62b90b60507b0c8dfbe373b
huittting <cat1181123@naver.com>
2022-01-24 20:36:32 +0900

    feat: 결제 도메인 구현

--- server/src/main/java/com/taxisharing/server/payment/domain/Payment.java
+++ server/src/main/java/com/taxisharing/server/payment/domain/Payment.java
@@ -1,12 +1,34 @@
 package com.taxisharing.server.payment.domain;
 
+import com.taxisharing.server.user.domain.User;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import javax.persistence.Entity;
+
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private Date time;
+
+    private Integer amount;
+
+    public Payment(Date time, Integer amount) {
+        this.time = time;
+        this.amount = amount;
+    }
+
+    public Payment(Integer amount){
+        this(new Date(System.currentTimeMillis()),amount);
+    }
+
 }
