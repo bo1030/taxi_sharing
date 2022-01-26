d0fa8c123d7bf8e6152ed294d4728010f1ef8730
Bochan kim <44803830+bo1030@users.noreply.github.com>
2022-01-26 15:00:53 +0900

    feat: authentication 설정 및 인터셉터 추가

---
+++ server/src/main/java/com/taxisharing/server/auth/config/AuthenticationInterceptor.java
@@ -0,0 +1,28 @@
+package com.taxisharing.server.auth.config;
+
+import com.taxisharing.server.auth.service.AuthenticationService;
+import com.taxisharing.server.auth.util.AuthorizationExtractor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationInterceptor implements HandlerInterceptor {
+
+    private final AuthenticationService authenticationService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        if ("OPTIONS".equals(request.getMethod())) {
+            return true;
+        }
+
+        String token = AuthorizationExtractor.extract(request);
+        authenticationService.validateToken(token);
+        return true;
+    }
+}
