f3ec76d487ce9921c6e18f5b3427e765b790e43f
dohkim <dh54kim54@gmail.com>
2022-01-30 23:36:14 +0900

    feat: request dto @ModelAttribute 명시

--- server/src/main/java/com/taxisharing/server/waiting_room/controller/WaitingRoomController.java
+++ server/src/main/java/com/taxisharing/server/waiting_room/controller/WaitingRoomController.java
@@ -3,10 +3,7 @@ package com.taxisharing.server.waiting_room.controller;
 import com.taxisharing.server.waiting_room.dto.ListRequest;
 import com.taxisharing.server.waiting_room.dto.ListResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
 
@@ -15,7 +12,7 @@ import javax.websocket.server.PathParam;
 public class WaitingRoomController {
 
     @GetMapping()
-    ResponseEntity<ListResponse> list(ListRequest listRequest) {
+    ResponseEntity<ListResponse> list(@ModelAttribute ListRequest listRequest) {
         String result = listRequest.toString();
         return ResponseEntity.ok(new ListResponse(result));
     }
