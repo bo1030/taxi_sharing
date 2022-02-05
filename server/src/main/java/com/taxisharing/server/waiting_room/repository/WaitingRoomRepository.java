550ce40b03e053701a996131139f329f1d69e836
Bochan kim <44803830+bo1030@users.noreply.github.com>
2022-02-05 12:24:09 +0900

    feat: WaitingRoomQueryRepository 추가

--- server/src/main/java/com/taxisharing/server/waiting_room/repository/WaitingRoomRepository.java
+++ server/src/main/java/com/taxisharing/server/waiting_room/repository/WaitingRoomRepository.java
@@ -2,12 +2,12 @@ package com.taxisharing.server.waiting_room.repository;
 
 import com.taxisharing.server.waiting_room.domain.WaitingRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.persistence.NamedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import java.util.List;
 
-public interface WaitingRoomRepository extends JpaRepository<WaitingRoom, Integer> {
-    List<WaitingRoom> findByDepartureLatitudeBetweenAndDepartureLongitudeBetween(Float leftLatitude,Float rightLatitude,Float leftLongitude,Float rightLongitude);
-    List<WaitingRoom> findByDestinationLatitudeBetweenAndDestinationLongitudeBetween(Float leftLatitude,Float rightLatitude,Float leftLongitude,Float rightLongitude);
+public interface WaitingRoomRepository extends JpaRepository<WaitingRoom, Integer>, WaitingRoomQueryRepository {
 
 }
