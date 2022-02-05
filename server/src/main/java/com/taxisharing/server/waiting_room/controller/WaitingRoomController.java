dbc0b9e54a20bf0a48e0a0a073cdfffbbac44298
dohkim <dh54kim54@gmail.com>
2022-02-05 09:57:48 +0900

    feat: repository added

--- server/src/main/java/com/taxisharing/server/waiting_room/controller/WaitingRoomController.java
+++ server/src/main/java/com/taxisharing/server/waiting_room/controller/WaitingRoomController.java
@@ -1,19 +1,18 @@
 package com.taxisharing.server.waiting_room.controller;
 
-import com.taxisharing.server.waiting_room.dto.ListRequest;
-import com.taxisharing.server.waiting_room.dto.ListResponse;
+import com.taxisharing.server.waiting_room.dto.WaitingRoomListRequest;
+import com.taxisharing.server.waiting_room.dto.WaitingRoomListResponse;
+import com.taxisharing.server.waiting_room.service.WaitingRoomService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/waiting-room")
 public class WaitingRoomController {
 
     @GetMapping()
-    ResponseEntity<ListResponse> list(@ModelAttribute ListRequest listRequest) {
-        String result = listRequest.toString();
-        return ResponseEntity.ok(new ListResponse(result));
+    ResponseEntity<WaitingRoomListResponse> waitingRoomList(@ModelAttribute WaitingRoomListRequest waitingRoomListRequest) {
+        return ResponseEntity.ok(waitingRoomService.getWaitingRoomList(waitingRoomListRequest));
     }
 }
