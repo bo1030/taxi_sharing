339104b1ed0b5c3c18d28d2982f422802aef3883
Bochan kim <44803830+bo1030@users.noreply.github.com>
2022-02-11 21:09:05 +0900

    feat: ArgumentResolver 추가

--- server/src/main/java/com/taxisharing/server/auth/config/AuthenticationConfig.java
+++ server/src/main/java/com/taxisharing/server/auth/config/AuthenticationConfig.java
@@ -2,14 +2,18 @@ package com.taxisharing.server.auth.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class AuthenticationConfig implements WebMvcConfigurer {
     private final AuthenticationInterceptor authenticationInterceptor;
+    private final ArgumentResolver argumentResolver;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -18,6 +22,12 @@ public class AuthenticationConfig implements WebMvcConfigurer {
                 .addPathPatterns("/user/*");
 
     }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(argumentResolver);
+    }
+
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
