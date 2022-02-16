7afe917bd1a8199d2dd1a140a627fde825a821c3
Bochan kim <44803830+bo1030@users.noreply.github.com>
2022-02-16 23:50:18 +0900

    refactor: 사용하지 않는 클래스 임포트 제거

--- server/src/main/java/com/taxisharing/server/common/util/S3Config.java
+++ server/src/main/java/com/taxisharing/server/common/util/S3Config.java
@@ -1,7 +1,6 @@
 package com.taxisharing.server.common.util;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
